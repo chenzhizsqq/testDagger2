3675e68d4c80c59c91b094f476ff1ca6f2bf607d
chenzhi <chenzhi@qq.com>
2022-02-07 17:48:14 +0900

    为了让在User @Inject constructor()中，加上构造值(var age:Int) 所以加上了@Module。和@Provides

--- app/src/main/java/com/example/testdagger2/MainActivity.kt
+++ app/src/main/java/com/example/testdagger2/MainActivity.kt
@@ -60,19 +60,54 @@ class MainActivity : AppCompatActivity() {
          * dog.toString(),在这里就说明了会调用 class Dog 的 fun toString()
          */
         Log.e("TAG", "dog: ${dog.toString()}")
+
+
+
+        /**
+         * dog.toString(),在这里就说明了会调用 class Dog 的 fun toString()
+         */
+        Log.e("TAG", "user 调用override fun toString(): : ${user.toString()}")
     }
 
+}
+
+/**
+ * @Module，修饰的类中所定义的有@Provides修饰的方法提供
+ */
+@Module
+class UserModule{
+    @Singleton
+    @Provides
+    fun provideUser():User
+    {
+        /**
+         * 说明了，调用provideUser()后，就表示初始化的构造值为User(28)
+         */
+        return User(28)
+    }
 }
 
 
 /**
  * 在User @Inject constructor(),就说明User类，是被Inject调用。
  */
-class User @Inject constructor()
+/*class User @Inject constructor()
 {
     var name:String?=null
     override fun toString(): String {
         return "Name:$name"
+    }
+}*/
+
+/**
+ * 在User @Inject constructor()中，加上构造值(var age:Int)，
+ * 需要在interface MainComponent中，加上@Component(modules = [UserModule::class])
+ */
+class User @Inject constructor(var age:Int)
+{
+    lateinit var name:String
+    override fun toString(): String {
+        return "Name:$name, Age:$age"
     }
 }
 
@@ -87,7 +122,22 @@ class Dog @Inject constructor(var owner:User)
 /**
  * 在MainComponent的接口中，就说明了在MainActivity中被调用
  */
-@Component
+/*@Component
 public interface MainComponent{
+    fun inject(activity: MainActivity)
+}*/
+
+
+/**
+ * 在User @Inject constructor()中，加上构造值，
+ * 需要在interface MainComponent中，加上@Component(modules = [UserModule::class])
+ */
+@Singleton
+/**
+ * @Component(modules = [UserModule::class])，
+ * 调了provideUser()，所以初始化的构造值为User(28)
+ */
+@Component(modules = [UserModule::class])
+interface MainComponent{
     fun inject(activity: MainActivity)
 }
